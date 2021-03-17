519f92f4c65bbfc12135020bf980e6f2827a8454
Raphael <paulberthet94@gmail.com>
2021-03-17 20:49:45 +0100

    17 mars 2021

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,18 @@
+class MobileMenu {
+  constructor() {
+
+    this.menuIcon = document.querySelector('.header__menu-icon');
+    this.menuContent = document.querySelector('.header__content');
+    this.events();
+  }
+
+  events() {
+    this.menuIcon.addEventListener('click', () => this.toggleTheMenu())
+  }
+
+  toggleTheMenu() {
+    this.menuContent.classList.toggle('header__content--is-visible')
+  }
+}
+
+export default MobileMenu;
