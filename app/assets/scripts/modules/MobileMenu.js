0c42fb76614681b023287919e5098948d8e70128
Raphael <paulberthet94@gmail.com>
2021-03-28 20:36:57 +0200

    header terminé

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,20 +1,22 @@
 
-// class MobileMenu {
-//   constructor() {
+class MobileMenu {
+  constructor() {
 
-//     this.menuIcon = document.querySelector('.header__menu-icon');
-//     this.menuContent = document.querySelector('.header__menu-content');
-//     this.events();
-//   }
+    this.menuIcon = document.querySelector('.header__menu-icon');
+    this.menuContent = document.querySelector('.header__menu-content');
+    this.header = document.querySelector('.header');
+    this.events();
+  }
 
-//   events() {
-//     this.menuIcon.addEventListener('click', () => this.toggleTheMenu())
-//   }
+  events() {
+    this.menuIcon.addEventListener('click', () => this.toggleTheMenu())
+  }
 
-//   toggleTheMenu() {
-//     console.log('test');
-//     this.menuContent.classList.toggle('header__menu-content--is-visible')
-//   }
-// }
+  toggleTheMenu() {
+    this.menuContent.classList.toggle('header__menu-content--is-visible')
+    this.header.classList.toggle('header--is-expanded')
+    this.menuIcon.classList.toggle('header__menu-icon--close-x')
+  }
+}
 
-// export default MobileMenu;
+export default MobileMenu;
